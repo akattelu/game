5994e3af416ffeec1982592cb190c42392018b55
Aakash Kattelu <aakash.kattelu@gmail.com>
2026-02-25 18:27:56 -0500

    init: add triangle sokol example

---
+++ build.zig
@@ -0,0 +1,60 @@
+const std = @import("std");
+const sokol = @import("sokol");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const dep_sokol = b.dependency("sokol", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    // extract shdc dependency from sokol dependency
+    const dep_shdc = dep_sokol.builder.dependency("shdc", .{});
+
+    // call shdc.createSourceFile() helper function, this returns a `!*Build.Step`:
+    const shdc_step = try sokol.shdc.createSourceFile(b, .{
+        .shdc_dep = dep_shdc,
+        .input = "src/triangle.glsl",
+        .output = "src/triangle.glsl.zig",
+        .slang = .{ .metal_macos = true, .spirv_vk = true },
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "game",
+        .root_module = b.createModule(.{
+            .root_source_file = b.path("src/main.zig"),
+            .target = target,
+            .optimize = optimize,
+            .imports = &.{
+                .{ .name = "sokol", .module = dep_sokol.module("sokol") },
+            },
+        }),
+    });
+    // add the shader compilation step as dependency to the build step
+    // which requires the generated Zig source file
+    exe.step.dependOn(shdc_step);
+
+    b.installArtifact(exe);
+
+    const run_step = b.step("run", "Run the app");
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_step.dependOn(&run_cmd.step);
+
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const exe_tests = b.addTest(.{
+        .root_module = exe.root_module,
+    });
+
+    const run_exe_tests = b.addRunArtifact(exe_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_exe_tests.step);
+}
---
+++ src/main.zig
@@ -0,0 +1,71 @@
+const std = @import("std");
+const sokol = @import("sokol");
+const slog = sokol.log;
+const sg = sokol.gfx;
+const sapp = sokol.app;
+const sglue = sokol.glue;
+
+const triangle_shader = @import("triangle.glsl.zig");
+
+const state = struct {
+    var pipeline: sg.Pipeline = .{};
+    var bind: sg.Bindings = .{};
+};
+
+export fn init() void {
+    sg.setup(.{ .environment = sglue.environment(), .logger = .{ .func = slog.func } });
+    // state.pass_action.colors[0] = .{
+    //     .load_action = .CLEAR,
+    //     .clear_value = .{ .r = 1, .g = 1, .b = 0, .a = 1 },
+    // };
+    //
+    state.bind.vertex_buffers[0] = sg.makeBuffer(.{
+        .data = sg.asRange(&[_]f32{
+            // positions         colors
+            0.0,  0.5,  0.5, 1.0, 0.0, 0.0, 1.0,
+            0.5,  -0.5, 0.5, 0.0, 1.0, 0.0, 1.0,
+            -0.5, -0.5, 0.5, 0.0, 0.0, 1.0, 1.0,
+        }),
+    });
+
+    state.pipeline = sg.makePipeline(.{
+        .shader = sg.makeShader(triangle_shader.triangleShaderDesc(sg.queryBackend())),
+        .layout = init: {
+            var l = sg.VertexLayoutState{};
+            l.attrs[triangle_shader.ATTR_triangle_position].format = .FLOAT3;
+            l.attrs[triangle_shader.ATTR_triangle_color0].format = .FLOAT4;
+            break :init l;
+        },
+    });
+}
+
+export fn frame() void {
+    // const g = state.pass_action.colors[0].clear_value.g + 0.01;
+    // state.pass_action.colors[0].clear_value.g = if (g > 1.0) 0.0 else g;
+
+    sg.beginPass(.{ .swapchain = sglue.swapchain() });
+    sg.applyPipeline(state.pipeline);
+    sg.applyBindings(state.bind);
+    sg.draw(0, 3, 1);
+    sg.endPass();
+    sg.commit();
+}
+
+export fn cleanup() void {
+    sg.shutdown();
+}
+
+pub fn main() !void {
+    sapp.run(.{
+        .init_cb = init,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+
+        .width = 640,
+        .height = 480,
+        .icon = .{ .sokol_default = true },
+        .window_title = "triangle.zig",
+
+        .logger = .{ .func = slog.func },
+    });
+}
