eab499299e321a2eaaa83a92e0e2cae5fd8e8300
Aakash Kattelu <aakash.kattelu@gmail.com>
2026-02-26 18:59:13 -0500

    feat: add rainbow color mesh

--- src/main.zig
+++ src/main.zig
@@ -4,6 +4,7 @@ const slog = sokol.log;
 const sg = sokol.gfx;
 const sapp = sokol.app;
 const sglue = sokol.glue;
+const sdtx = @import("sokol").debugtext;
 
 const math = @import("math.zig");
 const Mat4 = math.Mat4;
@@ -18,65 +19,89 @@ const state = struct {
     var pipeline: sg.Pipeline = .{};
     var bind: sg.Bindings = .{};
 
-    var eye: Vec3 = .{ .x = 0.0, .y = 1.5, .z = 5.0 };
+    var eye: Vec3 = .{ .x = 0.0, .y = 5.0, .z = 5.0 };
 };
 
 const Vertex = extern struct { x: f32, y: f32, z: f32, color: u32, u: i16, v: i16 };
 
+fn mapRange(value: f32, in_min: f32, in_max: f32, out_min: f32, out_max: f32) f32 {
+    return (value - in_min) / (in_max - in_min) * (out_max - out_min) + out_min;
+}
+
+fn rgbaToU32(r: u8, g: u8, b: u8, a: u8) u32 {
+    return @as(u32, a) << 24 | @as(u32, b) << 16 | @as(u32, g) << 8 | r;
+}
+
+inline fn vertices(comptime n: comptime_int) sg.Range {
+    var v: [n][n]Vertex = undefined;
+    const nf: f32 = @floatFromInt(n);
+    for (0..n) |i| {
+        for (0..n) |j| {
+            const r: u8 = @intFromFloat(mapRange(@floatFromInt(i), 0, n - 1, 0, 255));
+            const g: u8 = @intFromFloat(mapRange(@floatFromInt(j), 0, n - 1, 0, 255));
+            const b: u8 = 128;
+            const ir: f32 = @as(f32, @floatFromInt(i)) / (nf - 1);
+            _ = ir; // autofix
+            const jr: f32 = @as(f32, @floatFromInt(j)) / (nf - 1);
+            _ = jr; // autofix
+            v[i][j] = .{
+                .x = @as(f32, @floatFromInt(i)) - (nf / 2.0),
+                .y = 0,
+                .z = @as(f32, @floatFromInt(j)) - (nf / 2.0),
+                .color = rgbaToU32(r, g, b, 255),
+                .u = @intCast(@divTrunc(i, n)),
+                .v = @intCast(@divTrunc(j, n)),
+            };
+        }
+    }
+    return sg.asRange(&v);
+}
+
+inline fn indices(comptime n: comptime_int) sg.Range {
+    var idx: [(n - 1) * (n - 1) * 6]u16 = undefined;
+    var count: usize = 0;
+    for (0..n - 1) |iz| {
+        for (0..n - 1) |ix| {
+            const A: u16 = @intCast(iz * n + ix);
+            const B: u16 = @intCast(iz * n + ix + 1);
+            const C: u16 = @intCast((iz + 1) * n + ix);
+            const D: u16 = @intCast((iz + 1) * n + ix + 1);
+
+            idx[count + 0] = A;
+            idx[count + 1] = C;
+            idx[count + 2] = B;
+
+            idx[count + 3] = B;
+            idx[count + 4] = C;
+            idx[count + 5] = D;
+
+            count += 6;
+        }
+    }
+    return sg.asRange(&idx);
+}
 export fn init() void {
     sg.setup(.{ .environment = sglue.environment(), .logger = .{ .func = slog.func } });
 
-    state.bind.vertex_buffers[0] = sg.makeBuffer(.{
-        .data = sg.asRange(&[_]Vertex{
-            // zig fmt: off
-            .{ .x = -1.0, .y = -1.0, .z = -1.0, .color = 0xFF0000FF, .u = 0,     .v = 0 },
-            .{ .x =  1.0, .y = -1.0, .z = -1.0, .color = 0xFF0000FF, .u = 32767, .v = 0 },
-            .{ .x =  1.0, .y =  1.0, .z = -1.0, .color = 0xFF0000FF, .u = 32767, .v = 32767 },
-            .{ .x = -1.0, .y =  1.0, .z = -1.0, .color = 0xFF0000FF, .u = 0,     .v = 32767 },
-
-            .{ .x = -1.0, .y = -1.0, .z =  1.0, .color = 0xFF00FF00, .u = 0,     .v = 0 },
-            .{ .x =  1.0, .y = -1.0, .z =  1.0, .color = 0xFF00FF00, .u = 32767, .v = 0 },
-            .{ .x =  1.0, .y =  1.0, .z =  1.0, .color = 0xFF00FF00, .u = 32767, .v = 32767 },
-            .{ .x = -1.0, .y =  1.0, .z =  1.0, .color = 0xFF00FF00, .u = 0,     .v = 32767 },
-
-            .{ .x = -1.0, .y = -1.0, .z = -1.0, .color = 0xFFFF0000, .u = 0,     .v = 0 },
-            .{ .x = -1.0, .y =  1.0, .z = -1.0, .color = 0xFFFF0000, .u = 32767, .v = 0 },
-            .{ .x = -1.0, .y =  1.0, .z =  1.0, .color = 0xFFFF0000, .u = 32767, .v = 32767 },
-            .{ .x = -1.0, .y = -1.0, .z =  1.0, .color = 0xFFFF0000, .u = 0,     .v = 32767 },
-
-            .{ .x =  1.0, .y = -1.0, .z = -1.0, .color = 0xFFFF007F, .u = 0,     .v = 0 },
-            .{ .x =  1.0, .y =  1.0, .z = -1.0, .color = 0xFFFF007F, .u = 32767, .v = 0 },
-            .{ .x =  1.0, .y =  1.0, .z =  1.0, .color = 0xFFFF007F, .u = 32767, .v = 32767 },
-            .{ .x =  1.0, .y = -1.0, .z =  1.0, .color = 0xFFFF007F, .u = 0,     .v = 32767 },
-
-            .{ .x = -1.0, .y = -1.0, .z = -1.0, .color = 0xFFFF7F00, .u = 0,     .v = 0 },
-            .{ .x = -1.0, .y = -1.0, .z =  1.0, .color = 0xFFFF7F00, .u = 32767, .v = 0 },
-            .{ .x =  1.0, .y = -1.0, .z =  1.0, .color = 0xFFFF7F00, .u = 32767, .v = 32767 },
-            .{ .x =  1.0, .y = -1.0, .z = -1.0, .color = 0xFFFF7F00, .u = 0,     .v = 32767 },
-
-            .{ .x = -1.0, .y =  1.0, .z = -1.0, .color = 0xFF007FFF, .u = 0,     .v = 0 },
-            .{ .x = -1.0, .y =  1.0, .z =  1.0, .color = 0xFF007FFF, .u = 32767, .v = 0 },
-            .{ .x =  1.0, .y =  1.0, .z =  1.0, .color = 0xFF007FFF, .u = 32767, .v = 32767 },
-            .{ .x =  1.0, .y =  1.0, .z = -1.0, .color = 0xFF007FFF, .u = 0,     .v = 32767 },
-            // zig fmt: on
-        }),
+    sdtx.setup(.{
+        .fonts = init: {
+            var f: [8]sdtx.FontDesc = @splat(.{});
+            f[0] = sdtx.fontKc854();
+            break :init f;
+        },
+        .logger = .{ .func = slog.func },
     });
 
+    state.bind.vertex_buffers[0] = sg.makeBuffer(.{
+        .data = vertices(3),
+    });
 
     // cube index buffer
     state.bind.index_buffer = sg.makeBuffer(.{
         .usage = .{ .index_buffer = true },
-        .data = sg.asRange(&[_]u16{
-            0,  1,  2,  0,  2,  3,
-            6,  5,  4,  7,  6,  4,
-            8,  9,  10, 8,  10, 11,
-            14, 13, 12, 15, 14, 12,
-            16, 17, 18, 16, 18, 19,
-            22, 21, 20, 23, 22, 20,
-        }),
+        .data = indices(3),
     });
 
-    
     // create a small checker-board image and texture view
     state.bind.views[shd.VIEW_tex] = sg.makeView(.{
         .texture = .{
@@ -112,7 +137,7 @@ export fn init() void {
                 .compare = .LESS_EQUAL,
                 .write_enabled = true,
             },
-            .cull_mode = .BACK,
+            .cull_mode = .NONE,
         },
     );
 
@@ -130,10 +155,13 @@ export fn init() void {
 }
 
 export fn frame() void {
-    // const dt: f32 = @floatCast(sapp.frameDuration() * 60);
-    // state.rx += 1.0 * dt;
-    // state.ry += 2.0 * dt;
+    const dt: f32 = @floatCast(sapp.frameDuration() * 60);
+    // _ = dt; // autofix
+    state.rx += 1.0 * dt;
+    state.ry += 2.0 * dt;
     const vs_params = computeVsParams(state.rx, state.ry);
+
+    sdtx.print("Hello '{s}'!\n", .{"world"});
 
     sg.beginPass(.{
         .swapchain = sglue.swapchain(),
@@ -141,7 +169,8 @@ export fn frame() void {
     sg.applyPipeline(state.pipeline);
     sg.applyBindings(state.bind);
     sg.applyUniforms(shd.UB_vs_params, sg.asRange(&vs_params));
-    sg.draw(0, 36, 1);
+    sg.draw(0, 24, 1);
+    sdtx.draw();
     sg.endPass();
     sg.commit();
 }
@@ -150,38 +179,34 @@ export fn cleanup() void {
     sg.shutdown();
 }
 
-export fn event(e: [*c]const sapp.Event) callconv(.c) void{
+export fn event(e: [*c]const sapp.Event) callconv(.c) void {
     switch (e.*.type) {
         .KEY_DOWN => {
             switch (e.*.key_code) {
-                .LEFT =>  {
+                .LEFT => {
                     state.eye.y -= 0.8;
                 },
                 .RIGHT => {
                     state.eye.y += 0.8;
                 },
 
-                .UP =>  {
+                .UP => {
                     state.eye.z -= 0.8;
                 },
                 .DOWN => {
                     state.eye.z += 0.8;
                 },
 
-                .A =>  {
+                .A => {
                     state.eye.x -= 0.8;
                 },
-                .D=> {
+                .D => {
                     state.eye.x += 0.8;
                 },
-                else => {
-                    
-                }
+                else => {},
             }
         },
-        else => {
-            
-        }
+        else => {},
     }
 }
 
@@ -204,9 +229,12 @@ pub fn main() !void {
 }
 
 fn computeVsParams(rx: f32, ry: f32) shd.VsParams {
-    const rxm = Mat4.rotate(rx, .{ .x = 1.0, .y = 0.0, .z = 0.0 });
-    const rym = Mat4.rotate(ry, .{ .x = 0.0, .y = 1.0, .z = 0.0 });
-    const model = Mat4.mul(rxm, rym);
+    _ = rx; // autofix
+    _ = ry; // autofix
+    // const rxm = Mat4.rotate(rx, .{ .x = 1.0, .y = 0.0, .z = 0.0 });
+    // const rym = Mat4.rotate(ry, .{ .x = 0.0, .y = 1.0, .z = 0.0 });
+    // const model = Mat4.mul(rxm, rym);
+    const model = Mat4.identity();
     const aspect = sapp.widthf() / sapp.heightf();
     const proj = Mat4.persp(60.0, aspect, 0.01, 100.0);
     const view: Mat4 = Mat4.lookat(state.eye, Vec3.zero(), Vec3.up());
